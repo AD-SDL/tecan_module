941bdbb99b6b2398c1e4a614b67e759d447a44dc
Ryan Lewis <accounts+github@ryandlewis.dev>
2023-11-08 10:05:54 -0600

    Slight cleanup of test apparatus

--- test/run_test.py
+++ test/run_test.py
@@ -12,19 +12,8 @@ def main():
     exp = ExperimentClient("127.0.0.1", "8000", "Test_Tecan_Application")
     # This initializes the connection to the server and the logs for this run of the program.
     exp.register_exp()
-    # This runs the simulated_workflow a simulated workflow
-    flow_info = exp.start_run(wf_path.resolve(), simulate=False, blocking=True)
-    print(flow_info)
-    # This checks the state of the experiment in the queue
-    flow_status = exp.query_run(flow_info["run_id"])
-    # This will print out the queued job
-    print(flow_status)
-    # This will wait until the flow has run and then print out the final result of the flow
-    # while flow_status["status"] != WorkflowStatus.COMPLETED:
-    #     print(flow_status)
-    #     time.sleep(1)
-    #     flow_status = exp.query_run(flow_info["run_id"])
-    # print(flow_status)
+    # This runs the workflow and returns once the workflow is complete
+    exp.start_run(wf_path.resolve(), simulate=False, blocking=True)
 
 
 if __name__ == "__main__":
